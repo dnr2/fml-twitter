fdded9632d43ec97b228681def7c3b433679353b
dnr2 <dnr2@cin.ufpe.br>
2014-05-02 16:17:23 -0400

    training.py with adaboost, and svm. Using cross validation

--- train.py
+++ train.py
@@ -1,14 +1,24 @@
-from sklearn import svm
 import pandas as pd
 import numpy as np
 import pprint
+
+from sklearn import svm
+from sklearn import cross_validation
+from sklearn import datasets
+from sklearn.ensemble import AdaBoostClassifier
+
+
+model_type  = "adaboost" # svm
 
 data = np.genfromtxt('data.csv', delimiter=',')
 feature_array = np.delete(data, 0, 1)
 class_array = np.genfromtxt('data.csv', delimiter=',', usecols=([0]))
 
-pprint.pprint(feature_array)
-pprint.pprint(class_array)
+if model_type == "svm" :
+  clf = svm.SVC( kernel="poly", C=10, degree=3, verbose=True ).fit(feature_array, class_array)
+if model_type == "adaboost" :
+  clf = AdaBoostClassifier(n_estimators=200)
 
-classifier = svm.SVC(verbose=True)
-classifier.fit(feature_array, class_array)
+pprint.pprint( np.mean(cross_validation.cross_val_score(clf, feature_array, class_array, cv=10)) )
+
+
