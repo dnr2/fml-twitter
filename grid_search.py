d0c0ae7e9694feed9d03ba4a21fa9cc456fcc8d6
dnr2 <dnr2@cin.ufpe.br>
2014-05-02 17:11:22 -0400

    grid search printing the features and accuracies

--- grid_search.py
+++ grid_search.py
@@ -20,33 +20,40 @@ features = [
 "created_at",
 "geo_enabled",
 "hashtag_count",
-"symbols_count",
-"urls_count",
-"user_mentions_count",
-"favorite_count",
-"retweet_count",
 "hashtag_avg",
+"symbols_count",
 "symbols_avg",
+"urls_count",
 "urls_avg",
+"user_mentions_count",
 "user_mentions_avg",
+"favorite_count",
 "favorite_avg",
+"retweet_count",
 "retweet_avg",
 ]
 
+sorted_features  = {}
 
+class_array = np.genfromtxt('data.csv', delimiter=',', usecols=([0]))
 
-for column in range( 1, 19 ) :
-  data = np.genfromtxt('train.csv', delimiter=',')
-  feature_array = np.delete(data, 0, 1)
-  pprint.pprint(feature_array)
-  feature_array = np.delete(feature_array, 0, column)
-  pprint.pprint(feature_array)
-  class_array = np.genfromtxt('train.csv', delimiter=',', usecols=([0]))
-
+for column in range( 1, 20 ) :
+  print features[column]
+  # get only one column of the training data
+  feature_array  = np.genfromtxt('data.csv', delimiter=',', usecols=([0,column]))
+  feature_array = np.delete(feature_array, 0, 1)
+  
+  pprint.pprint( feature_array )
+  
   if model_type == "svm" :
     clf = svm.SVC( kernel="poly", C=10, degree=3, verbose=True ).fit(feature_array, class_array)
   if model_type == "adaboost" :
-    clf = AdaBoostClassifier(n_estimators=200)
+    clf = AdaBoostClassifier(n_estimators=10)
 
-  print features[column]
-  pprint.pprint( np.mean(cross_validation.cross_val_score(clf, feature_array, class_array, cv=10)) )
+  accuracy =  np.mean(cross_validation.cross_val_score(clf, feature_array, class_array, cv=10)) 
+  sorted_features[features[column]] = accuracy
+  pprint.pprint( accuracy )
+
+for key in sorted_features.keys():
+  print str(key) + " " + str(sorted_features[key])
+  
