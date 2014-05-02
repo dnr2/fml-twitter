e1b87e5ea6bcaa55ac13f170f88f7cad8f5d629c
Cody Coats <cpcoats@gmail.com>
2014-05-02 16:35:18 -0400

    added feature title list

--- grid_search.py
+++ grid_search.py
@@ -10,19 +10,43 @@ from sklearn.ensemble import AdaBoostClassifier
 
 model_type  = "adaboost" # adaboost / svm / ...
 
+features = [
+"verified",
+"followers_count",
+"friends_count",
+"listed_count",
+"statuses_count",
+"contributors_enabled",
+"created_at",
+"geo_enabled",
+"hashtag_count",
+"symbols_count",
+"urls_count",
+"user_mentions_count",
+"favorite_count",
+"retweet_count",
+"hashtag_avg",
+"symbols_avg",
+"urls_avg",
+"user_mentions_avg",
+"favorite_avg",
+"retweet_avg",
+]
+
+
 
 for column in range( 1, 19 ) :
-  data = np.genfromtxt('data.csv', delimiter=',')
+  data = np.genfromtxt('train.csv', delimiter=',')
   feature_array = np.delete(data, 0, 1)
-  feature_array = np.delete(data, column, 1)
-  class_array = np.genfromtxt('data.csv', delimiter=',', usecols=([0]))
+  pprint.pprint(feature_array)
+  feature_array = np.delete(feature_array, 0, column)
+  pprint.pprint(feature_array)
+  class_array = np.genfromtxt('train.csv', delimiter=',', usecols=([0]))
 
   if model_type == "svm" :
     clf = svm.SVC( kernel="poly", C=10, degree=3, verbose=True ).fit(feature_array, class_array)
   if model_type == "adaboost" :
     clf = AdaBoostClassifier(n_estimators=200)
-  
-  print column
-  pprint.pprint( np.mean(cross_validation.cross_val_score(clf, feature_array, class_array, cv=10)) )
-  
 
+  print features[column]
+  pprint.pprint( np.mean(cross_validation.cross_val_score(clf, feature_array, class_array, cv=10)) )
