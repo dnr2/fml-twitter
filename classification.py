b70198baf5773a10cedcb04bc7eaecb541ce0cd2
Cody Coats <cpcoats@gmail.com>
2014-05-08 19:33:09 -0400

    added precision recall code to classify.py and added more data

--- classification.py
+++ classification.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pylab as pl
 import pprint
 
 from sklearn import svm
@@ -10,11 +11,15 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn import tree
+from sklearn import svm, datasets
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import auc
 
 
 model_type  = "adaboost" # adaboost / svm / stochastic_gradient_descent / nearestneighbor / decision_tree ...
 cv_folds = 10 #number of cross validation folds
 use_CV = False #use cross validation
+create_PR = True
 
 training_data = np.genfromtxt('traindata.csv', delimiter=',')
 np.random.shuffle(training_data)
@@ -41,3 +46,21 @@ if use_CV :
   pprint.pprint( np.mean(cross_validation.cross_val_score(clf, training_X, training_Y, cv=cv_folds)) )
 else :
   pprint.pprint( clf.fit(training_X, training_Y).score(testing_X , testing_Y) )
+if create_PR:
+  # Run classifier
+  probas_ = clf.fit(training_X, training_Y).predict_proba(testing_X)
+
+  # Compute Precision-Recall and plot curve
+  precision, recall, thresholds = precision_recall_curve(testing_Y, probas_[:, 1])
+  area = auc(recall, precision)
+  print("Area Under Curve: %0.2f" % area)
+
+  pl.clf()
+  pl.plot(recall, precision, label='Precision-Recall curve')
+  pl.xlabel('Recall')
+  pl.ylabel('Precision')
+  pl.ylim([0.0, 1.05])
+  pl.xlim([0.0, 1.0])
+  pl.title('Precision-Recall example: AUC=%0.2f' % area)
+  pl.legend(loc="lower left")
+  pl.show()
