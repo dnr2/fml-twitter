7dfe876fd3c4f9616c04a4d0bc37f6fbca4e998c
Cody Coats <cpcoats@gmail.com>
2014-05-09 18:14:28 -0400

    added serializaiton of classifier

--- classification.py
+++ classification.py
@@ -20,9 +20,10 @@ from sklearn.externals import joblib
 
 
 names = ["adaboost","stochastic_gradient_descent","nearestneighbor","decision_tree"]
+name = "adaboost"
 #model_type  = "adaboost" # adaboost / svm / stochastic_gradient_descent / nearestneighbor / decision_tree ...
 cv_folds = 10 #number of cross validation folds
-use_CV = True #use cross validation
+use_CV = False #use cross validation
 create_PR = False
 save_Classifer = True
 
@@ -55,10 +56,22 @@ if use_CV :
         a[name] = [np.mean(cross_validation.cross_val_score(clf, training_X, training_Y, cv=cv_folds))]
         print name,a[name]
 
-#pprint.pprint( np.mean(cross_validation.cross_val_score(clf, training_X, training_Y, cv=cv_folds)) )
-
-else :
+else:
+  if name == "svm" :
+      clf = svm.SVC( kernel="poly", C=10, degree=3, verbose=True )
+  if name == "adaboost" :
+      clf = AdaBoostClassifier(n_estimators=200)
+  if name == "stochastic_gradient_descent" :
+      clf = SGDClassifier(loss="hinge", penalty="l2")
+  if name == "nearestneighbor" :
+      clf = NearestCentroid()
+  if name == "decision_tree" :
+      clf = tree.DecisionTreeClassifier()
   pprint.pprint( clf.fit(training_X, training_Y).score(testing_X , testing_Y) )
+#pprint.pprint( np.mean(cross_validation.cross_val_score(clf, training_X, training_Y, cv=cv_folds)) )
+if save_Classifer:
+  joblib.dump(clf, name+'_classifier.pkl', compress=3)
+
 if create_PR:
   # Run classifier
   probas_ = clf.fit(training_X, training_Y).predict_proba(testing_X)
@@ -77,5 +90,3 @@ if create_PR:
   pl.title('Precision-Recall example: AUC=%0.2f' % area)
   pl.legend(loc="lower left")
   pl.show()
-if save_Classifer:
-  joblib.dump(clf, name+'_classifier.pkl') 
